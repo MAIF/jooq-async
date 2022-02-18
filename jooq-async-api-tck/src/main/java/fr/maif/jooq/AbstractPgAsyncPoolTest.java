277242690b96004c1a9f2b6b98e2b2029d28f9bf
Alexandre Delègue <adelegue@hotmail.com>
2022-02-18 15:03:01 +0100

    Upgrade jooq / vertx

--- jooq-async-api-tck/src/main/java/fr/maif/jooq/AbstractPgAsyncPoolTest.java
+++ jooq-async-api-tck/src/main/java/fr/maif/jooq/AbstractPgAsyncPoolTest.java
@@ -25,43 +25,46 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.postgresql.ds.PGSimpleDataSource;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicReference;
 
 import io.vavr.jackson.datatype.VavrModule;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import static io.vavr.API.Some;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.jooq.impl.DSL.field;
+import static org.jooq.impl.SQLDataType.TIMESTAMP;
 
 public abstract class AbstractPgAsyncPoolTest {
 
+    private final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres").withTag("14"));
+
     private final static ObjectMapper mapper = new ObjectMapper();
+    private PgAsyncPool pgAsyncPool;
+
     {
         mapper.registerModule(new VavrModule());
     }
+
     protected ActorSystem actorSystem = ActorSystem.create("test", ConfigFactory.parseString(
             "jdbc-execution-context {\n" +
-            "  type = Dispatcher\n" +
-            "  executor = \"thread-pool-executor\"\n" +
-            "  throughput = 1\n" +
-            "  thread-pool-executor {\n" +
-            "    fixed-pool-size = 5\n" +
-            "  }\n" +
-            "}"));
-    protected final static String user = "eventsourcing";
-    protected final static String password = "eventsourcing";
-    protected final static String database = "eventsourcing";
-    protected final static Integer port = 5557;
-    protected final static String url = "jdbc:postgresql://localhost:"+port+"/"+database;
+                    "  type = Dispatcher\n" +
+                    "  executor = \"thread-pool-executor\"\n" +
+                    "  throughput = 1\n" +
+                    "  thread-pool-executor {\n" +
+                    "    fixed-pool-size = 5\n" +
+                    "  }\n" +
+                    "}"));
     protected PGSimpleDataSource dataSource;
     protected DSLContext dslContext;
-    protected final static AtomicReference<PGSimpleDataSource> dataSourceRef = new AtomicReference<>();
 
-    public abstract PgAsyncPool pgAsyncPool();
+    public abstract PgAsyncPool pgAsyncPool(PostgreSQLContainer<?> postgreSQLContainer);
 
     private Random seq = new Random();
 
@@ -71,30 +74,30 @@ public abstract class AbstractPgAsyncPoolTest {
     protected Table<Record> table;
     protected Field<String> name = field("name", String.class);
     protected Field<JsonNode> meta = field("meta", JSON);
-    protected Field<Timestamp> created = field("created", Timestamp.class);
-
-    @BeforeClass
-    public static void initDS() {
-        if (dataSourceRef.get() == null) {
-            PGSimpleDataSource dataSource = new PGSimpleDataSource();
-            dataSource.setUrl(url);
-            dataSource.setUser(user);
-            dataSource.setPassword(password);
-            dataSourceRef.set(dataSource);
-        }
-    }
+    protected Field<Timestamp> created = field("created", TIMESTAMP);
+    protected Field<BigDecimal> bigDecimal = field("number", BigDecimal.class);
 
     @Before
     public void setUp() {
+        System.out.println("Starting postgresql");
+        postgreSQLContainer.start();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        System.out.println("Postgresql has started on port "+postgreSQLContainer.getJdbcUrl());
+        dataSource = new PGSimpleDataSource();
+        dataSource.setUrl(postgreSQLContainer.getJdbcUrl());
+        dataSource.setUser(postgreSQLContainer.getUsername());
+        dataSource.setPassword(postgreSQLContainer.getPassword());
         this.number = seq.nextInt(10000);
         this.table = DSL.table("viking_async" + number);
-        this.dataSource = dataSourceRef.get();
         this.dslContext = DSL.using(dataSource, SQLDialect.POSTGRES, new Settings());
+        System.out.println("Creating tables");
         try {
             dslContext.createTableIfNotExists(this.table)
                     .column(name)
                     .column(meta)
                     .column(created)
+                    .column(bigDecimal)
                     .execute();
 
             dslContext.createTableIfNotExists(Person.PERSON)
@@ -102,103 +105,114 @@ public abstract class AbstractPgAsyncPoolTest {
                     .column(Person.PERSON.METADATA)
                     .column(Person.PERSON.CREATED)
                     .execute();
-        } catch(Exception e) {}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("Tables created");
+        pgAsyncPool = pgAsyncPool(postgreSQLContainer);
     }
 
     @After
     public void cleanUp() {
         try {
             this.dslContext.dropTable(table).execute();
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
         try {
             this.dslContext.dropTable(Person.PERSON).execute();
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
+        postgreSQLContainer.stop();
     }
 
     @Test
     public void insertInTransaction() {
-        Future<Integer> insertResult = pgAsyncPool().inTransaction(t ->
+        Future<Integer> insertResult = pgAsyncPool.inTransaction(t ->
                 t.execute(dsl -> dsl.insertInto(table).set(name, "test"))
         );
         insertResult.get();
 
-        List<QueryResult> result = pgAsyncPool().query(dsl -> dsl.select(name).from(table)).get();
+        List<QueryResult> result = pgAsyncPool.query(dsl -> dsl.select(name).from(table)).get();
         assertThat(result).hasSize(1);
     }
 
     @Test
     public void insertInTransactionWithRollback() {
         assertThatThrownBy(() ->
-                pgAsyncPool().inTransaction(t -> t
-                        .execute(dsl -> dsl.insertInto(table).set(name, "test"))
-                        .mapTry(__ -> { throw new RuntimeException("Oups"); })
-                )
-                .get()
+                pgAsyncPool.inTransaction(t -> t
+                                .execute(dsl -> dsl.insertInto(table).set(name, "test"))
+                                .mapTry(__ -> {
+                                    throw new RuntimeException("Oups");
+                                })
+                        )
+                        .get()
         ).hasMessage("Oups");
 
-        List<QueryResult> result = pgAsyncPool().query(dsl -> dsl.select(name).from(table)).get();
+        List<QueryResult> result = pgAsyncPool.query(dsl -> dsl.select(name).from(table)).get();
         assertThat(result).isEmpty();
     }
 
     @Test
     public void insertManualTransaction() {
-        pgAsyncPool().begin().flatMap(t -> t
+        pgAsyncPool.begin().flatMap(t -> t
                 .execute(dsl -> dsl.insertInto(table).set(name, "test"))
                 .onSuccess(__ -> t.commit())
                 .onFailure(__ -> t.rollback())
         ).get();
 
-        List<QueryResult> result = pgAsyncPool().query(dsl -> dsl.select(name).from(table)).get();
+        List<QueryResult> result = pgAsyncPool.query(dsl -> dsl.select(name).from(table)).get();
         assertThat(result).hasSize(1);
     }
 
     @Test
     public void insertManualTransactionWithRollback() {
         assertThatThrownBy(() ->
-                pgAsyncPool().begin().flatMap(t -> t
+                pgAsyncPool.begin().flatMap(t -> t
                         .execute(dsl -> dsl.insertInto(table).set(name, "test"))
-                        .mapTry(__ -> { throw new RuntimeException("Oups"); })
+                        .mapTry(__ -> {
+                            throw new RuntimeException("Oups");
+                        })
                         .onSuccess(__ -> t.commit())
                         .onFailure(__ -> t.rollback())
                 ).get()
         ).hasMessage("Oups");
 
-        List<QueryResult> result = pgAsyncPool().query(dsl -> dsl.select(name).from(table)).get();
+        List<QueryResult> result = pgAsyncPool.query(dsl -> dsl.select(name).from(table)).get();
         assertThat(result).isEmpty();
     }
 
     @Test
     public void executeBatchAndReadMany() {
         List<String> names = List.range(0, 10).map(i -> "name-" + i);
-        Future<Long> batchResult = pgAsyncPool().executeBatch(
+        Future<Long> batchResult = pgAsyncPool.executeBatch(
                 dsl -> dslContext.insertInto(table).columns(name).values((String) null),
                 names.map(List::of)
         );
         batchResult.get();
-        List<String> result = pgAsyncPool().query(dsl -> dsl.select(name).from(table)).get().map(q -> q.get(name));
+        List<String> result = pgAsyncPool.query(dsl -> dsl.select(name).from(table)).get().map(q -> q.get(name));
         assertThat(result).containsExactly(names.toJavaArray(String[]::new));
     }
 
     @Test
     public void executeBatch2AndReadMany() {
         List<String> names = List.range(0, 10).map(i -> "name-" + i);
-        Future<Long> batchResult = pgAsyncPool().executeBatch(dsl ->
+        Future<Long> batchResult = pgAsyncPool.executeBatch(dsl ->
                 names.map(n -> dslContext.insertInto(table).set(name, n))
         );
         batchResult.get();
-        List<String> result = pgAsyncPool().query(dsl -> dsl.select(name).from(table)).get().map(q -> q.get(name));
+        List<String> result = pgAsyncPool.query(dsl -> dsl.select(name).from(table)).get().map(q -> q.get(name));
         assertThat(result).containsExactly(names.toJavaArray(String[]::new));
     }
 
     @Test
     public void queryOne() {
-        pgAsyncPool().executeBatch(dsl ->
+        pgAsyncPool.executeBatch(dsl ->
                 List.range(0, 10).map(i -> "name-" + i).map(n -> dslContext.insertInto(table).set(name, n))
         ).get();
 
-Future<Option<String>> futureResult = pgAsyncPool()
-        .queryOne(dsl -> dsl.select(name).from(table).where(name.eq("name-1")))
-        .map(mayBeResult -> mayBeResult.map(row -> row.get(name)));
+        Future<Option<String>> futureResult = pgAsyncPool
+                .queryOne(dsl -> dsl.select(name).from(table).where(name.eq("name-1")))
+                .map(mayBeResult -> mayBeResult.map(row -> row.get(name)));
         Option<String> res = futureResult
                 .get();
         assertThat(res).isEqualTo(Some("name-1"));
@@ -206,7 +220,7 @@ Future<Option<String>> futureResult = pgAsyncPool()
 
     @Test
     public void queryOneEmpty() {
-        Option<String> res = pgAsyncPool().queryOne(dsl -> dsl.select(name).from(table).where(name.eq("name-1"))).get().map(q -> q.get(name));
+        Option<String> res = pgAsyncPool.queryOne(dsl -> dsl.select(name).from(table).where(name.eq("name-1"))).get().map(q -> q.get(name));
         assertThat(res).isEmpty();
     }
 
@@ -214,15 +228,15 @@ Future<Option<String>> futureResult = pgAsyncPool()
     public void queryOneAsJson() {
 
 
-        pgAsyncPool().executeBatch(dsl ->
+        pgAsyncPool.executeBatch(dsl ->
                 List.range(0, 10).map(i -> dslContext
                         .insertInto(table)
-                        .set(name, "name-"+i)
-                        .set(meta, jsonFromMap(HashMap.of("name", "A name "+i)))
+                        .set(name, "name-" + i)
+                        .set(meta, jsonFromMap(HashMap.of("name", "A name " + i)))
                 )
         ).get();
 
-        Future<Option<JsonNode>> futureResult = pgAsyncPool()
+        Future<Option<JsonNode>> futureResult = pgAsyncPool
                 .queryOne(dsl -> dsl.select(meta).from(table).where(name.eq("name-1")))
                 .map(mayBeResult -> mayBeResult.map(row -> row.get(meta)));
 
@@ -233,16 +247,16 @@ Future<Option<String>> futureResult = pgAsyncPool()
     @Test
     public void queryOneAsTimestamp() {
         LocalDateTime localDateTime = LocalDateTime.of(2019, 1, 1, 0, 0, 0, 0);
-        pgAsyncPool().executeBatch(dsl ->
+        pgAsyncPool.executeBatch(dsl ->
                 List.range(0, 10).map(i -> dslContext
                         .insertInto(table)
-                        .set(name, "name-"+i)
-                        .set(meta, jsonFromMap(HashMap.of("name", "A name "+i)))
+                        .set(name, "name-" + i)
+                        .set(meta, jsonFromMap(HashMap.of("name", "A name " + i)))
                         .set(created, Timestamp.valueOf(localDateTime))
                 )
         ).get();
 
-        Future<Option<Timestamp>> futureResult = pgAsyncPool()
+        Future<Option<Timestamp>> futureResult = pgAsyncPool
                 .queryOne(dsl -> dsl.select(created).from(table).where(name.eq("name-1")))
                 .map(mayBeResult -> mayBeResult.map(row -> row.get(created)));
 
@@ -252,13 +266,33 @@ Future<Option<String>> futureResult = pgAsyncPool()
 
 
     @Test
+    public void queryOneAsBigDecimal() {
+        BigDecimal bd = new BigDecimal("1.5");
+        pgAsyncPool.executeBatch(dsl ->
+                List.range(0, 10).map(i -> dslContext
+                        .insertInto(table)
+                        .set(name, "name-"+i)
+                        .set(bigDecimal, bd)
+                )
+        ).get();
+
+        Future<Option<BigDecimal>> futureResult = pgAsyncPool
+                .queryOne(dsl -> dsl.select(bigDecimal).from(table).where(name.eq("name-1")))
+                .map(mayBeResult -> mayBeResult.map(row -> row.get(bigDecimal)));
+
+        Option<BigDecimal> res = futureResult.get();
+        assertThat(res).isEqualTo(Some(bd));
+    }
+
+
+    @Test
     public void stream() {
         List<String> names = List.range(0, 10000).map(i -> "name-" + i);
-        pgAsyncPool().executeBatch(dsl ->
+        pgAsyncPool.executeBatch(dsl ->
                 names.map(n -> dslContext.insertInto(table).set(name, n))
         ).get();
 
-        Source<String, NotUsed> stream = pgAsyncPool()
+        Source<String, NotUsed> stream = pgAsyncPool
                 .stream(10, dsl -> dsl.select(name).from(table))
                 .map(q -> q.get(name));
         List<String> res = stream
@@ -278,7 +312,7 @@ Future<Option<String>> futureResult = pgAsyncPool()
                 .set(record)
                 .execute();
 
-        final PersonRecord result = pgAsyncPool().query(dsl -> dsl.selectFrom(Person.PERSON))
+        final PersonRecord result = pgAsyncPool.query(dsl -> dsl.selectFrom(Person.PERSON))
                 .get()
                 .head()
                 .toRecord(Person.PERSON);
@@ -297,7 +331,7 @@ Future<Option<String>> futureResult = pgAsyncPool()
                 .set(record)
                 .execute();
 
-        final PersonRecord result = pgAsyncPool().query(dsl -> dsl.selectFrom(Person.PERSON))
+        final PersonRecord result = pgAsyncPool.query(dsl -> dsl.selectFrom(Person.PERSON))
                 .get()
                 .head()
                 .toRecord(new PersonRecord(Person.PERSON));
--- jooq-async-reactive/src/test/java/fr/maif/jooq/ReactiveAsyncPoolTest.java
+++ jooq-async-reactive/src/test/java/fr/maif/jooq/ReactiveAsyncPoolTest.java
@@ -5,46 +5,44 @@ import io.vertx.core.Vertx;
 import io.vertx.pgclient.PgConnectOptions;
 import io.vertx.pgclient.PgPool;
 import io.vertx.sqlclient.PoolOptions;
+import org.jooq.Configuration;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DefaultConfiguration;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-
-import java.util.concurrent.atomic.AtomicReference;
+import org.junit.After;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 public class ReactiveAsyncPoolTest extends AbstractPgAsyncPoolTest {
 
     private static Vertx vertx = Vertx.vertx();
-    private static AtomicReference<ReactivePgAsyncPool> thePoolRef = new AtomicReference<>();
-    private static AtomicReference<PgPool> clientRef = new AtomicReference<>();
+    private PgPool pool;
 
     @Override
-    public PgAsyncPool pgAsyncPool() {
-        return thePoolRef.get();
-    }
+    public PgAsyncPool pgAsyncPool(PostgreSQLContainer<?> postgreSQLContainer) {
+        Configuration jooqConfig = new DefaultConfiguration().set(SQLDialect.POSTGRES);
+        String host = postgreSQLContainer.getHost();
+        Integer port = postgreSQLContainer.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT);
+        String databaseName = postgreSQLContainer.getDatabaseName();
+        String username = postgreSQLContainer.getUsername();
+        String password = postgreSQLContainer.getPassword();
 
-    @AfterClass
-    public static void atTheEnd() {
-        if (clientRef.get() != null) {
-            clientRef.get().close();
-            clientRef.set(null);
-        }
-    }
+        System.out.println(String.format("Reactive pool %s %d %s %s %s", host, port, databaseName, username, password));
 
-    @BeforeClass
-    public static void createPool() {
-        DefaultConfiguration jooqConfig = new DefaultConfiguration();
-        jooqConfig.setSQLDialect(SQLDialect.POSTGRES);
         PgConnectOptions options = new PgConnectOptions()
                 .setPort(port)
-                .setHost("localhost")
-                .setDatabase(database)
-                .setUser(user)
+                .setHost(host)
+                .setDatabase(databaseName)
+                .setUser(username)
                 .setPassword(password);
         PoolOptions poolOptions = new PoolOptions().setMaxSize(3);
-        clientRef.set(PgPool.pool(vertx, options, poolOptions));
+        pool = PgPool.pool(vertx, options, poolOptions);
+        var connection = pool.getConnection().toCompletionStage().toCompletableFuture().join();
+        connection.close().toCompletionStage().toCompletableFuture().join();
+        return new ReactivePgAsyncPool(pool, jooqConfig);
+    }
 
-        thePoolRef.set(new ReactivePgAsyncPool(clientRef.get(), jooqConfig));
+    @After
+    public void atTheEnd() {
+        pool.close();
     }
 
 }
