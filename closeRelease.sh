65073c49a0d853c44d00905f8257b9b5a0901477
Alexandre Delègue <adelegue@hotmail.com>
2022-09-22 16:51:00 +0200

    CI

--- closeRelease.sh
+++ closeRelease.sh
@@ -1,4 +1,6 @@
-if [[ ${{ github.event.head_commit.message }} = "[Gradle Release Plugin] - pre tag commit"* ]]
+#!/bin/bash
+
+if [[ $1 == "[Gradle Release Plugin] - pre tag commit"* ]]
 then
   echo "Closing release"
   ./gradlew closeAndReleaseRepository
